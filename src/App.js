72cb5e572b261cb608ae56b41b4e26f0592698b4
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-11-19 18:29:10 +0100

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Markup, Editor, Container, Column, Row, RuleInput, RuleLabel, StyleInput, Button,
+ Document} from './styled'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Column>
+          <Button>
+            New Rule
+          </Button>
+        </Column>
+        <Column>
+          <Button>Random Text</Button>
+          <Document>
+            <Editor />
+            <Markup />
+          </Document>
+        </Column>
+      </Container>
     );
   }
 }
