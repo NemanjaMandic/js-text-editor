96bfbf9fef52ec83b77faa8cf7d46be7c193e35f
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-11-20 14:35:16 +0100

    new function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
 
 
   state = {
-    editor: " "
+    editor: '',
+    name0: '',
+    begin0: '',
+    end0: '',
+    style0: '',
+    rules: 1
   }
 
   handleChange = (event) =>{
@@ -15,25 +20,44 @@ class App extends Component {
       [name]: value
     })
   }
-  render() {
-    const favoriteToppings = {
-  penautButer: 'kruncy',
-  jelly: 'stravbery'
-}
 
-const sendvic = {
-  bredslajs: 2,
-  ...favoriteToppings
-}
-    console.log(sendvic);
+ rules = () => {
+
+ }
+  newFields = () => {
+    this.setState((prevState) => {
+      let {rules} = prevState;
+      let fields = ['name', 'beign', 'end', 'style'];
+      let inputValues = {};
+
+      fields.forEach( (field) => {
+        inputValues = {
+          ...inputValues,
+          [`${field}${rules}`]: ''
+        }
+      })
+
+      rules++
+      console.log({
+        rules,
+        ...inputValues
+      })
+      return{
+        rules,
+        ...inputValues
+      }
+    })
+  }
+  render() {
+
     //object assembly shorthand
     let {value} = this.state;
-    let { handleChange } = this;
+    let { handleChange, newFields, rules } = this;
 
     return (
       <Container>
         <Column>
-          <Button>
+          <Button onClick={ newFields }>
             New Rule
           </Button>
         </Column>
